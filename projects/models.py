9826eda947f68a70cce3f9b73ebae7a8e1cb7ecb
Sebastian- <hmurgu@hotmail.com>
2019-12-11 13:53:37 -0800

    create project and technology models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    repo_url = models.URLField('Repository URL', blank=True)
+    site_url = models.URLField('Site URL', max_length=2000, blank=True)
+    image = models.ImageField()
+    technologies = models.ManyToManyField(
+        'Technology', related_name='projects')
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100)
